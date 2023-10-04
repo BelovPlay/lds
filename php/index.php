1708de1f69ec7c94d865fac0efc9aae26ad80af1
Danya <valerij.belusin@gmail.com>
2023-10-04 21:25:36 +0300

    add code

--- php/index.php
+++ php/index.php
@@ -8,11 +8,8 @@
 <body>
 <?php 
 // Данные с инпутов
-$id=$_POST['Id'];
-$name=$_POST['Name'];
-$mail=$_POST['Email'];
-$phone=$_POST['Phone'];
-$numticket=$_POST['NumTicket'];
+$name=$_POST['name'];
+$phone=$_POST['phone'];
 // Данные для БД
 $servername="localhost";
 $username="root";
@@ -23,7 +20,7 @@ if($conn->connect__error){
     echo 'Подключение не удалось';
     die('Connection Failed:'.$conn->connect__error);
 }
-$sql="INSERT INTO users (Name,Email,Phone,NumTicket)
+$sql="INSERT INTO users (Name,'',Phone,'');
 VALUES ('$name','$mail','$phone','$numticket')";
 if($conn->query($sql)===TRUE){
     echo "Данные отправлены";
