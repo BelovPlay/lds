36ce7f740bc41be268a3a9c482d864fcf33ea331
Danya <valerij.belusin@gmail.com>
2023-10-04 21:07:49 +0300

    add code

--- php/index.php
+++ php/index.php
@@ -6,8 +6,32 @@
     <title>Отправка данных интеграторам</title>
 </head>
 <body>
-    <?php 
-        
-    ?>
+<?php 
+// Данные с инпутов
+$id=$_POST['Id'];
+$name=$_POST['Name'];
+$mail=$_POST['Email'];
+$phone=$_POST['Phone'];
+$numticket=$_POST['NumTicket'];
+// Данные для БД
+$servername="localhost";
+$username="root";
+$password="";
+$dbname='clients';
+$conn=new mysqli($servername,$username,$password,$dbname);
+if($conn->connect__error){
+    echo 'Подключение не удалось';
+    die('Connection Failed:'.$conn->connect__error);
+}
+$sql="INSERT INTO users (Name,Email,Phone,NumTicket)
+VALUES ('$name','$mail','$phone','$numticket')";
+if($conn->query($sql)===TRUE){
+    echo "Данные отправлены";
+}else{
+    echo "Error: " . $sql . '<br>' . $conn->$error;
+    echo 'Данные не отправлены';
+}
+$conn->close();
+?>
 </body>
 </html>
