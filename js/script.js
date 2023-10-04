787c329c19aa515f42ed81d26ee4a2d1beecfdf8
Danya <valerij.belusin@gmail.com>
2023-10-04 20:50:13 +0300

    add code

--- js/script.js
+++ js/script.js
@@ -6,9 +6,6 @@ calc.addEventListener('click',function(){
     event.preventDefault();
     let answercalc=confirm('Вы хотите открыть калькулятор для расчета стоимости проекта?');
     if(answercalc){
-        alert('Калькулятор пока не доведен до конца, но скоро появится :) Следите за новостями');
-    }else{
-        // alert('Я показываю калькулятор');
         calcmodal.style.display='block';
         overlay.style.display='block';
     }
