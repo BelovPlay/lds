d3f5bb89749dd7a375dbaa9dbf3b313b3175a563
Danya <valerij.belusin@gmail.com>
2023-10-04 20:29:24 +0300

    add pages

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let calc=document.querySelector('.calc');
 let calcmodal=document.querySelector('.calcmodal');
 let overlay=document.querySelector('.overlay');
+let integration=document.getElementById('integration');
 calc.addEventListener('click',function(){
     event.preventDefault();
     let answercalc=confirm('Вы хотите открыть калькулятор для расчета стоимости проекта?');
@@ -11,4 +12,7 @@ calc.addEventListener('click',function(){
         calcmodal.style.display='block';
         overlay.style.display='block';
     }
+})
+integration.addEventListener('click',function(){
+    
 })
